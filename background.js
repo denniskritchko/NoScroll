346b3ce7ab965cfc51f6daa217f3afae4de86456
Dennis Kritchko <dennis.kritchko@gmail.com>
2025-02-04 22:27:02 -0800

    fixed notification bug and changed images

--- background.js
+++ background.js
@@ -1,21 +1,26 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.alarms.create("reminder", { periodInMinutes: 0.5 });
+    chrome.alarms.create("reminder", { periodInMinutes: 0.1 });
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
     if (alarm.name === "reminder") {
         chrome.storage.sync.get("tasks", function (data) {
             if (data.tasks && data.tasks.length > 0) {
-                const url = chrome.runtime.getURL("notifications.html");
-                chrome.windows.create({
-                    url: url,
-                    type: "popup",
-                    width: 300,
-                    height: 150,
-                    top: 10,
-                    left: screen.availWidth - 310
-                })
+                const url = chrome.runtime.getURL("notification.html");
+                chrome.system.display.getInfo((displays) => {
+                    const primaryDisplay = displays.find(display => display.isPrimary === true) || displays[0];
+                    const screenWidth = primaryDisplay.workArea.width;
+                    const screenHeight = primaryDisplay.workArea.height;
 
+                    chrome.windows.create({
+                        url: url,
+                        type: "popup",
+                        width: 350,
+                        height: 150,
+                        top: 10,
+                        left: screenWidth - 310
+                    });
+                });
             }
         });
     }
