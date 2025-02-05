8587458617b08c0dce67a2e911c1a31c9ae83eee
Dennis Kritchko <dennis_kritchko@sfu.ca>
2025-02-04 19:49:21 -0800

    switching IDEs

--- background.js
+++ background.js
@@ -7,8 +7,15 @@ chrome.alarms.onAlarm.addListener((alarm) => {
         chrome.storage.sync.get("tasks", function (data) {
             if (data.tasks && data.tasks.length > 0) {
                 const url = chrome.runtime.getURL("notifications.html");
-                window.open(url, "Reminder", "width=300,height=150,top=10,right=10");
-                
+                chrome.windows.create({
+                    url: url,
+                    type: "popup",
+                    width: 300,
+                    height: 150,
+                    top: 10,
+                    left: screen.availWidth - 310
+                })
+
             }
         });
     }
