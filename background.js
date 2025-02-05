cdf552835a1eb208fae511786a05c28fcd48723e
Dennis Kritchko <dennis_kritchko@sfu.ca>
2025-02-04 19:42:43 -0800

    added notifications in HTML instead of chrome api

--- background.js
+++ background.js
@@ -1,18 +1,14 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.alarms.create("reminder", {periodInMinutes: 30});
+    chrome.alarms.create("reminder", { periodInMinutes: 0.5 });
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-    if (alarm.name == "reminder") {
+    if (alarm.name === "reminder") {
         chrome.storage.sync.get("tasks", function (data) {
             if (data.tasks && data.tasks.length > 0) {
-                chrome.notifications.create({
-                    type: "basic",
-                    iconUrl: "icon.png",
-                    title: "To-Do Reminder",
-                    message: "Don't forget you tasks! " + data.tasks.join(", "),
-                    priority: 2    
-                });
+                const url = chrome.runtime.getURL("notifications.html");
+                window.open(url, "Reminder", "width=300,height=150,top=10,right=10");
+                
             }
         });
     }
